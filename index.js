083dbd2de08b02f59e9972fec6774b66b348f25c
hoc vu <hocvu2003@gmail.com>
2025-02-17 22:17:08 +0700

    add: database

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = require("./app");
+const { Sequelize } = require('sequelize');
+const sequelize = require('./db');
+require("dotenv").config();
+
+// Kiểm tra kết nối
+sequelize.authenticate()
+  .then(() => {
+    console.log('Database connected...');
+    return sequelize.sync(); // Tạo bảng tự động
+  })
+  .then(() => {
+    console.log('Sequelize models synchronized.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server running on port 3000!");
+});
+
+module.exports = sequelize;
