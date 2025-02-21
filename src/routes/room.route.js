b64c29f141a60bcb704aa726a18fccd6094ea329
hoc vu <hocvu2003@gmail.com>
2025-02-21 17:27:48 +0700

    build: update room route

--- src/routes/room.route.js
+++ src/routes/room.route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const roomRouter = express.Router();
+const roomController = require('../controllers/room.controller');
+roomRouter.route('/').get(roomController.getRooms).post(roomController.addRoom);
 
-roomRouter.route('/').get().post();
-
-roomRouter.route('/id').get().patch().delete();
+roomRouter
+  .route('/id')
+  .get(roomController.getRoom)
+  .patch(roomController.updateRoom)
+  .delete(roomController.deleteRoom);
 
 module.exports = roomRouter;
